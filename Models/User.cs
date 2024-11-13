58e426035c87436bcaa1192404676dbe735146eb
Tribog <julix4212@gmail.com>
2024-11-13 18:06:21 -0500

    feat: The folder and model class "User" were created.

---
+++ Models/User.cs
@@ -0,0 +1,46 @@
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HeladeriaMondongo.Models;
+[Table("users")]
+public class User
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
+    public int Id { get; set; }
+
+    [Column("name")]
+    [MaxLength(250)]
+    public string Name { get; set; }
+
+    [Column("last_name")]
+    [MaxLength(250)]
+    public string LastName { get; set; }
+
+    [Column("email")]
+    [MaxLength(250)]
+    public string Email { get; set; }
+
+    [Column("password")]
+    [MaxLength(220)]
+    public string Password { get; set; }
+
+    [Column("is_admin")]
+    public bool IsAdmin { get; set; }
+
+
+    public User(string name, string email, string password, bool isAdmin)
+    {
+        Name = name.ToLower().Trim(); ;
+        Email = email.ToLower().Trim(); ;
+        Password = password;
+        IsAdmin = isAdmin; 
+    }
+
+    public User()
+    {
+
+    }
+}
