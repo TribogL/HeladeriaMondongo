e6192bde005c411cff40235f787fce5640a43537
MajoPino <majo12pino@gmail.com>
2024-11-14 07:06:50 -0500

    feat: Data annotations were added to the icecream model.

--- Models/Icecream.cs
+++ Models/Icecream.cs
@@ -1,15 +1,33 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace HeladeriaMondongo.Models;
 
+[Table("icecreams")]
 public class Icecream
 {
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    [Column("id")]
     public int Id { get; set; }
+
+    [Column("name")]
+    [Required]
+    [MaxLength(100)]
     public required string Name { get; set; }
+
+    [Column("description")]
+    [Required]
+    [MaxLength(255)]
     public required string Description { get; set; }
+
+    [Column("price")]
+    [Required]
+    [Range(2500.00, 25000.00)]
     public required double Price { get; set; }
 
 }
