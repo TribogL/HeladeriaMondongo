54b03bd104e5f0f7673f7823c36f06ad056f89a6
MajoPino <majo12pino@gmail.com>
2024-11-14 08:16:33 -0500

    feat: Configuration of the environment variables on the Program.cs file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
+using DotNetEnv;
+using HeladeriaMondongo.Data;
+using Microsoft.EntityFrameworkCore;
+
+Env.Load();
+//Create the environment variables
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+var dbName = Environment.GetEnvironmentVariable("DB_NAME");
+var dbUsername = Environment.GetEnvironmentVariable("DB_USERNAME");
+var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+//Configuration of the connection string
+var connectionString = $"server={dbHost};port={dbPort};database={dbName};uid={dbUsername};password={dbPassword}";
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseMySql(connectionString, ServerVersion.Parse("8.0.80-mysql")));
 
 // Add services to the container.
 
